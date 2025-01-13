ec36ee23523cffa8bbc1a4b227e41cb775a01ce4
Ryan Har <ryan.har@amus.dev>
2025-01-13 21:29:48 +0800

    Increase config code coverage

--- src/util/config.spec.ts
+++ src/util/config.spec.ts
@@ -3,17 +3,20 @@ import fs from 'fs';
 import path from 'path';
 import { describe, expect, test } from 'vitest';
 
-import { getWpConfig, getWpConfigByPath } from './config';
+import { getWpConfig, getWpConfigByPath, setupInitialWpConfig } from './config';
 
 describe('getWpConfigByPath()', () => {
   test('should call getWpConfig()', () => {
     const result = getWpConfigByPath(path.join(appRootPath, 'test/wordpress'));
-    console.log(result);
+    expect(result.dbHost).toBe('localhost');
+    expect(result.dbPort).toBe(3306);
+    expect(result.dbUser).toBe('username_here');
+    expect(result.database).toBe('database_name_here');
   });
 });
 
 describe('getWpConfig()', () => {
-  test('parse wordpress config and get correct result', () => {
+  test('parse wordpress sample config and get correct result', () => {
     const wpConfigSample = fs.readFileSync(
       path.join(appRootPath, 'test/wordpress/wp-config-sample.php'),
       'utf8'
@@ -23,5 +26,39 @@ describe('getWpConfig()', () => {
     expect(result.dbPort).toBe(3306);
     expect(result.dbUser).toBe('username_here');
     expect(result.database).toBe('database_name_here');
+  });
+  test('parse wordpress config and get correct result', () => {
+    const wpConfigSample = fs.readFileSync(
+      path.join(appRootPath, 'test/wordpress/wp-config.php'),
+      'utf8'
+    );
+    const result = getWpConfig(wpConfigSample);
+    expect(result.dbHost).toBe('database.host.local');
+    expect(result.dbPort).toBe(3389);
+    expect(result.dbUser).toBe('username');
+    expect(result.database).toBe('new_database');
+  });
+});
+
+describe('setupInitialWpConfig()', () => {
+  test('should inject correct parameters', () => {
+    const wpConfigSample = fs.readFileSync(
+      path.join(appRootPath, 'test/wordpress/wp-config-sample.php'),
+      'utf8'
+    );
+    const wpConfig = fs.readFileSync(
+      path.join(appRootPath, 'test/wordpress/wp-config.php'),
+      'utf8'
+    );
+    const newWpConfig = setupInitialWpConfig(
+      wpConfigSample,
+      'database.host.local',
+      3389,
+      'username',
+      'password',
+      'new_database',
+      'test_passphrase'
+    );
+    expect(newWpConfig).equal(wpConfig);
   });
 });
